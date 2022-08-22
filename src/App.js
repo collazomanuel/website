f91787f80d7397c5f4289719c0322ebc7c282f57
Manuel Collazo <collazo.manuel.28@gmail.com>
2022-08-22 17:44:07 -0300

    more features

--- src/App.js
+++ src/App.js
@@ -1,25 +1,171 @@
-import logo from './logo.svg';
-import './App.css';
+import { useContext } from 'react'
+import { ThemeContext } from './contexts/theme'
+import Header from './components/Header/Header'
+import About from './components/About/About'
+import Projects from './components/Projects/Projects'
+import Skills from './components/Skills/Skills'
+import ScrollToTop from './components/ScrollToTop/ScrollToTop'
+import Contact from './components/Contact/Contact'
+import Footer from './components/Footer/Footer'
+import './App.css'
 
-function App() {
+import Particles from "react-tsparticles";
+
+import { loadFull } from "tsparticles";
+
+const App = () => {
+
+  const [{ themeName }] = useContext(ThemeContext)
+
+  const particlesInit = async (main) => {
+    console.log(main);
+    await loadFull(main);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='top' className={`${themeName} app`}>
+
+      <div id='container'>
+
+        <Particles
+          id="tsparticles"
+          init={particlesInit}
+
+          options={{
+            "fullScreen": {
+                "enable": false,
+                "zIndex": 1
+            },
+            "particles": {
+                "number": {
+                    "value": 10,
+                    "density": {
+                        "enable": true,
+                        "value_area": 800
+                    }
+                },
+                "color": {
+                    "value": "#e3dcc2"
+                },
+                "shape": {
+                    "type": "circle",
+                    "options": {
+                        "sides": 5
+                    }
+                },
+                "opacity": {
+                    "value": 0,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 0.5,
+                        "opacity_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "size": {
+                    "value": 4,
+                    "random": false,
+                    "anim": {
+                        "enable": false,
+                        "speed": 20,
+                        "size_min": 0.1,
+                        "sync": false
+                    }
+                },
+                "rotate": {
+                    "value": 0,
+                    "random": true,
+                    "direction": "clockwise",
+                    "animation": {
+                        "enable": true,
+                        "speed": 2,
+                        "sync": false
+                    }
+                },
+                "line_linked": {
+                    "enable": true,
+                    "distance": 600,
+                    "color": "#253B2F",
+                    "opacity": 0.4,
+                    "width": 2
+                },
+                "move": {
+                    "enable": true,
+                    "speed": 1,
+                    "direction": "none",
+                    "random": false,
+                    "straight": false,
+                    "out_mode": "out",
+                    "attract": {
+                        "enable": false,
+                        "rotateX": 600,
+                        "rotateY": 1200
+                    }
+                }
+            },
+            "interactivity": {
+                "events": {
+                    "onhover": {
+                        "enable": true,
+                        "mode": ["grab"]
+                    },
+                    "onclick": {
+                        "enable": false,
+                        "mode": "bubble"
+                    },
+                    "resize": true
+                },
+                "modes": {
+                    "grab": {
+                        "distance": 400,
+                        "line_linked": {
+                            "opacity": 1
+                        }
+                    },
+                    "bubble": {
+                        "distance": 400,
+                        "size": 40,
+                        "duration": 2,
+                        "opacity": 8,
+                        "speed": 3
+                    },
+                    "repulse": {
+                        "distance": 200
+                    },
+                    "push": {
+                        "particles_nb": 4
+                    },
+                    "remove": {
+                        "particles_nb": 2
+                    }
+                }
+            },
+            "retina_detect": true,
+            "background": {
+                "color": "f0e8ce",
+                "image": "",
+                "position": "50% 50%",
+                "repeat": "no-repeat",
+                "size": "cover",
+                "opacity": "0%",
+            }
+        }}
+        />
+        <Header/>
+        <About />
+      </div>
+
+
+      <Projects />
+      <Skills />
+      <Contact />
+
+
+      <ScrollToTop />
+      <Footer />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/portfolio.js
@@ -0,0 +1,61 @@
+const header = {
+  homepage: 'https://collazomanuel.github.io/website',
+  title: 'MC.',
+}
+
+const about = {
+  name: 'Manuel Collazo',
+  role: 'Programmer',
+  description: 'Electronic Engineering student at the University of Buenos Aires with a focus on the area of Digital Systems and Computing.',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://linkedin.com',
+    github: 'https://github.com',
+  },
+}
+
+const projects = [
+  {
+    name: 'Project 1',
+    description:
+      'An online multiplayer RTS game made in C++17, no engine',
+    stack: ['C++', 'SDL2'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 2',
+    description:
+      'A WebGL 3D engine made in Vanilla JS',
+    stack: ['JavaScript', 'WebGL'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://github.com',
+  },
+  {
+    name: 'Project 3',
+    description:
+      'A system to automate the regulation of temperature and moisture of a culture chamber for mushrooms',
+    stack: ['C++', 'Arduino'],
+    sourceCode: 'https://github.com',
+    livePreview: 'https://www.researchgate.net/publication/356904406_Construction_de_una_camara_de_cultivo_altamente_especializada_y_de_bajo_costo_para_uso_de_laboratorio',
+  },
+]
+
+const skills = [
+  'C/C++',
+  'Java',
+  'Smalltalk',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React',
+  'Node',
+  'Git',
+  'CI/CD'
+]
+
+const contact = {
+  email: 'collazo.manuel@outlook.com',
+}
+
+export { header, about, projects, skills, contact }
